85464f774c18877ae35c496012b4f7732312d9a8
M Yusril Ihza Mahendra <yusrilza201@gmail.com>
2024-05-20 13:42:13 +0000

    feat: Insert prompt usernmae

--- number_guess.sh
+++ number_guess.sh
@@ -8,6 +8,24 @@ MAIN_MENU() {
     echo -e "\n$1"
   fi
 
+  #get username
+  echo -e "\nEnter your username:"
+  read USERNAME
+
+  #query from username_result by username
+  USERNAME_RESULT=$($PSQL "SELECT username FROM username WHERE username='$USERNAME'")
+
+    #if not found
+  if [[ -z $USERNAME_RESULT ]]
+  then
+    echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+    
+    #insert username
+    INSERT_USERNAME_RESULT=$($PSQL "INSERT INTO username(username) VALUES('$USERNAME')")
+
+  else
+    
+
 }
 
 NUMBER_GUESSING_GENERATOR() {
@@ -15,3 +33,5 @@ NUMBER_GUESSING_GENERATOR() {
   NUMBER=$(( RANDOM % 100 + 1 ))
 
 }
+
+MAIN_MENU
