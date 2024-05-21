fa23ec8c3953cb95bf36f0d6d0b882acea96b2bc
M Yusril Ihza Mahendra <yusrilza201@gmail.com>
2024-05-21 15:39:51 +0000

    feat: update best game user

--- number_guess.sh
+++ number_guess.sh
@@ -24,7 +24,7 @@ MAIN_MENU() {
     INSERT_USERNAME_PROMPT_RESULT=$($PSQL "INSERT INTO username(username) VALUES('$USERNAME_PROMPT')")
 
     #update games_played
-    UPDATE_GAME_PLAYED=$($PSQL "UPDATE username SET game_played=1 WHERE username='$USERNAME_PROMPT'")
+    UPDATE_GAME_PLAYED=$($PSQL "UPDATE username SET games_played=1 WHERE username='$USERNAME_PROMPT'")
 
   else
     #get username, games_played, best_game
@@ -34,7 +34,7 @@ MAIN_MENU() {
       echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
       
       #update games_played
-      UPDATE_GAME_PLAYED=$($PSQL "UPDATE username SET game_played=game_played+1 WHERE username='$USERNAME_PROMPT'")
+      UPDATE_GAME_PLAYED=$($PSQL "UPDATE username SET games_played=games_played+1 WHERE username='$USERNAME_PROMPT'")
     done
   fi
 
@@ -78,6 +78,22 @@ NUMBER_GUESSING_GENERATOR() {
     else
       echo -e "\nYou guessed it in $NUMBER_TRY tries. The secret number was $SECRET_NUMBER"
       break
+    fi
+  done
+
+  #get username, games_played, best_game
+  DATA_RESULT=$($PSQL "SELECT username, games_played, best_game FROM username WHERE username='$USERNAME_PROMPT'")
+  echo "$DATA_RESULT" | while IFS="|" read USERNAME GAMES_PLAYED BEST_GAME
+  do
+    #update best game
+    if [[ -z $BEST_GAME ]]
+    then
+      UPDATE_BEST_GAME=$($PSQL "UPDATE username SET best_game=$NUMBER_TRY WHERE username='$USERNAME'")
+    elif [[ $NUMBER_TRY -gt $BEST_GAME ]]
+    then
+      UPDATE_BEST_GAME=$($PSQL "UPDATE username SET best_game=$NUMBER_TRY WHERE username='$USERNAME'")
+    else :
+      #break from the loop
     fi
   done
 
