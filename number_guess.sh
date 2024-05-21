151e7af391d36868bbf6c2b1b75aa089b0496101
M Yusril Ihza Mahendra <yusrilza201@gmail.com>
2024-05-21 14:30:57 +0000

    feat: insert username exist prompt

--- number_guess.sh
+++ number_guess.sh
@@ -10,21 +10,30 @@ MAIN_MENU() {
 
   #get username
   echo -e "\nEnter your username:"
-  read USERNAME
+  read USERNAME_PROMPT
 
-  #query from username_result by username
-  USERNAME_RESULT=$($PSQL "SELECT username FROM username WHERE username='$USERNAME'")
+  #query from username_result by username_prompt
+  USERNAME_RESULT=$($PSQL "SELECT username FROM username WHERE username='$USERNAME_PROMPT'")
 
     #if not found
   if [[ -z $USERNAME_RESULT ]]
   then
-    echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+    echo -e "\nWelcome, $USERNAME_PROMPT! It looks like this is your first time here."
     
     #insert username
-    INSERT_USERNAME_RESULT=$($PSQL "INSERT INTO username(username) VALUES('$USERNAME')")
-  fi
-    
+    INSERT_USERNAME_PROMPT_RESULT=$($PSQL "INSERT INTO username(username) VALUES('$USERNAME_PROMPT')")
 
+    else
+      #get username, games_played, best_game
+      DATA_RESULT=$($PSQL "SELECT username, games_played, best_game FROM username WHERE username='$USERNAME_PROMPT'")
+      echo "$DATA_RESULT" | while IFS="|" read USERNAME GAMES_PLAYED BEST_GAME
+      do
+        echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+      done
+  fi
+
+  #go to number guessing games
+  NUMBER_GUESSING_GENERATOR
 }
 
 NUMBER_GUESSING_GENERATOR() {
